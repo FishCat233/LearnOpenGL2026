598abbe2dd9fba09ff29145c5a85c1265fa5c71c
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-03-09 00:00:46 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window, double& speed) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+		speed = 1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+		speed = 5;
+	}
+	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+		speed = 10;
+	}
+
+}
+
+int main() {
+	// 初始化 GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// 创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// 初始化 GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// 视口设置
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	double speed = 1;
+
+	// 渲染循环
+	while (!glfwWindowShouldClose(window)) {
+		auto time = glfwGetTime();
+
+		auto r = (sin(time * speed) * 0.5f) + 0.5f;
+		auto g = (sin(time * speed + 2.0943) * 0.5f) + 0.5f;
+		auto b = (sin(time * speed + 4.1886) * 0.5f) + 0.5f;
+
+		//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(r, g, b, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		processInput(window, speed);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+	glfwTerminate();
+
+	return 0;
+}
