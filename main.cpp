dc9ba7cffa1783dabeaf377e1ac09a4db9ade263
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-03-09 18:00:16 +0800

    feat(day2): ！？三角形？！

--- main.cpp
+++ main.cpp
@@ -24,6 +24,84 @@ void processInput(GLFWwindow* window, double& speed) {
 
 }
 
+int CreateShader(unsigned int& shader, const char* shaderSource, GLenum shaderType) {
+	shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &shaderSource, NULL);
+	glCompileShader(shader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+void DrawATriangle() {
+	float vertices[] = {
+		 -0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f, 0.5f, 0.0f
+	};
+
+	// 着色器
+	unsigned int vertexShader;
+	const char* vertexShaderSource = "#version 330 core\n" "layout (location = 0) in vec3 aPos;\n" "void main()\n" "{\n" " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" "}\0";
+	{
+		auto err = CreateShader(vertexShader, vertexShaderSource, GL_VERTEX_SHADER);
+		if (err) {
+			std::cout << "CreateVertexShader failed" << std::endl;
+			return;
+		}
+	}
+
+	unsigned int fragmentShader;
+	const char* fragmentShaderSource = "#version 330 core\n" "out vec4 FragColor;\n" "void main()\n" "{\n" " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" "}\0";
+	{
+		auto err = CreateShader(fragmentShader, fragmentShaderSource, GL_FRAGMENT_SHADER);
+		if (err) {
+			std::cout << "CreateFragmentShader failed" << std::endl;
+			return;
+		}
+	}
+
+	// 创建着色器程序
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// glGetProgramiv
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// VBO
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // ?
+
+	// 使用程序
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glEnableVertexAttribArray(0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+}
+
 int main() {
 	// 初始化 GLFW
 	glfwInit();
@@ -68,6 +146,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		processInput(window, speed);
+
+		DrawATriangle();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
